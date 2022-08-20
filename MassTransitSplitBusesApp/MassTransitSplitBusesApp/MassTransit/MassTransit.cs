f40491b16bc19c4dca1d153044ca3c8a7d4ed01e
Jan Kowalczuk <jankow97@gmail.com>
2022-08-20 12:14:50 +0200

    Fix

---
+++ MassTransitSplitBusesApp/MassTransitSplitBusesApp/MassTransit/MassTransit.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+
+namespace MassTransitSplitBusesApp.MassTransit;
+
+public interface ISecondBus :
+    IBus
+{
+}
+
+public class TestMessage2
+{
+    public string? Message2 { get; set; }
+}
+
+public class TestConsumer2 : IConsumer<TestMessage2>
+{
+    public static int RequestCount;
+
+    public Task Consume(ConsumeContext<TestMessage2> context)
+    {
+        RequestCount++;
+        return Task.CompletedTask;
+    }
+}
+
+public class TestMessage
+{
+    public string? Message { get; set; }
+}
+
+public class TestConsumer : IConsumer<TestMessage>
+{
+    public static int RequestCount;
+
+    public Task Consume(ConsumeContext<TestMessage> context)
+    {
+        RequestCount++;
+        return Task.CompletedTask;
+    }
+}
--- MassTransitSplitBusesApp/MassTransitSplitBusesApp/RabbitExtenstions.cs
+++ MassTransitSplitBusesApp/MassTransitSplitBusesApp/RabbitExtenstions.cs
@@ -1,5 +1,5 @@
-﻿
-using MassTransit;
+﻿using MassTransit;
+using MassTransitSplitBusesApp.MassTransit;
 
 namespace MassTransitSplitBusesApp;
 
@@ -56,42 +56,5 @@ internal static class RabbitExtensions
         });
 
         return services;
-    }
-
-    public interface ISecondBus :
-        IBus
-    {
-    }
-
-    public class TestMessage2
-    {
-        public string Message2 { get; set; }
-    }
-
-    public class TestConsumer2 : IConsumer<TestMessage2>
-    {
-        public static int RequestCount;
-
-        public Task Consume(ConsumeContext<TestMessage2> context)
-        {
-            RequestCount++;
-            return Task.CompletedTask;
-        }
-    }
-
-    public class TestMessage
-    {
-        public string Message { get; set; }
-    }
-
-    public class TestConsumer : IConsumer<TestMessage>
-    {
-        public static int RequestCount;
-
-        public Task Consume(ConsumeContext<TestMessage> context)
-        {
-            RequestCount++;
-            return Task.CompletedTask;
-        }
     }
 }
